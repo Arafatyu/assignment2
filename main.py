a1d6d4356c0d542213d8c28f6840804425b8b9a4
Arafatyu <arafat1800@gmail.com>
2026-09-15 15:40:56 -0400

    added main

--- main.py
+++ main.py
@@ -17,4 +17,3 @@ if __name__ == "__main__":
     # Now, we use the calculator tool we got earlier. This will start the calculator, which is a program 
     # that keeps running and doing math based on what we tell it.
     calculator()
-
